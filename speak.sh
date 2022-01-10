5963b1a5daddd89e3733fd37903e60dcfe786a48
Jorge (George) <ct1jib@gmail.com>
2022-01-10 17:03:02 +0000

    Update speak.sh

--- speak.sh
+++ speak.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/espeak -vde "Hier ist D B 0 W M S in Schöppinge" --stdout | aplay --device=mycard
+/usr/bin/espeak -vde "Aqui está C Q 0 E D M A em Setubal" --stdout | aplay --device=mycard
