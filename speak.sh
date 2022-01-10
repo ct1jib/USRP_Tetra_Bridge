8a45c65d185713742a5e5357f502fc40079e2e5c
Jorge (George) <ct1jib@gmail.com>
2022-01-10 18:27:19 +0000

    Update speak.sh

--- speak.sh
+++ speak.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/espeak -v pt F -s 140 "Está no C Q 0 E D M A repetidor Tetra em Setubal" --stdout | aplay --device=mycard
+/usr/bin/espeak -v pt_pt -s 140 "Está no C Q 0 E D M A repetidor Tetra em Setubal" --stdout | aplay --device=mycard
